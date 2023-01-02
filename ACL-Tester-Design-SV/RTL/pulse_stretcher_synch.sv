c55f139525e92245b5669fe5eeb2c3e3045fb19b
Timothy Stotts <timothystotts08@gmail.com>
2023-01-02 15:52:25 -0500

    Various comments, comment style, and tab style were changed for pulse_stretcher_synch entity and modules.

--- ACL-Tester-Design-SV/RTL/pulse_stretcher_synch.sv
+++ ACL-Tester-Design-SV/RTL/pulse_stretcher_synch.sv
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------------------
 -- MIT License
 --
--- Copyright (c) 2020-2021 Timothy Stotts
+-- Copyright (c) 2020-2022 Timothy Stotts
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
@@ -30,92 +30,92 @@
 -- the FSM of Figure 8.28a from the text Finite State Machines in Hardware:
 -- Theory and Design (with VHDL and SystemVerilog) by Volnei A. Pedroni..
 ------------------------------------------------------------------------------*/
-`begin_keywords "1800-2009"
+`begin_keywords "1800-2012"
 //Timed Moore machine with timer control strategy #1
 //Part 1: Module header:--------------------------------------------------------
 module pulse_stretcher_synch
-	#(parameter
-		/* The exact count of clock cycles to hold Y as a one immediately after
-		   a single clock cycle of X being a value of one. */
-		integer par_T_stretch_bits = 7, /* LOG2 of \ref par_T_stretch_val */
-		integer par_T_stretch_val = 64
-		)
-	(
-		/* The stretched output */
-		output logic o_y,
-		/* Clock and reset */
-		input logic i_clk,
-		input logic i_rst,
-		/* The input value to stretch upon value of one. */
-		input logic i_x
-		);
+    #(parameter
+        // The exact count of clock cycles to hold Y as a one immediately after
+        // a single clock cycle of X being a value of one.
+        integer par_T_stretch_bits = 7, // LOG2 of \ref par_T_stretch_val
+        integer par_T_stretch_val = 64
+        )
+    (
+        // The stretched output
+        output logic o_y,
+        // Clock and reset
+        input logic i_clk,
+        input logic i_rst,
+        // The input value to stretch upon value of one.
+        input logic i_x
+        );
 
 // Part 2: Declarations---------------------------------------------------------
 timeunit 1ns;
 timeprecision 1ps;
 
-/* Stretcher FSM state values */
+// Stretcher FSM state values
 localparam [0:0] ST_A = 1'b0;
 localparam [0:0] ST_B = 1'b1;
 
-/* Stretcher FSM time constants */
+// Stretcher FSM time constants
 localparam [(par_T_stretch_bits - 1):0] c_t_stretch = par_T_stretch_val;
 localparam [(par_T_stretch_bits - 1):0] c_tmax = c_t_stretch - 1;
 
-/* Stretcher FSM state register and next state signal */
-(* fsm_encoding = "gray" *)
+// Stretcher FSM state register and next state signal
+(* fsm_encoding = "auto" *)
 (* fsm_safe_state = "default_state" *)
 logic [0:0] s_stretch_pr_state;
 logic [0:0] s_stretch_nx_state;
 
-/* Stretcher FSM timing signal */
+// Stretcher FSM timing signal
 logic [(par_T_stretch_bits - 1):0] s_t;
 
 //Part 3: Statements------------------------------------------------------------
-/* Timer with strategy #1 implementation. The timer re-zeroes on a Stretch FSM
-   state transition; and it caps at the constant parameter c_tmax. */
+// Timer with strategy #1 implementation. The timer re-zeroes on a Stretch FSM
+// state transition; and it caps at the constant parameter c_tmax.
 always_ff @(posedge i_clk)
 begin: p_fsm_timer
-	if (i_rst)
-		s_t <= 0;
-	else
-		if (s_stretch_pr_state != s_stretch_nx_state) begin : if_chg_state
-			s_t <= 0;
-		end : if_chg_state
+    if (i_rst)
+        s_t <= 0;
+    else
+        if (s_stretch_pr_state != s_stretch_nx_state) begin : if_chg_state
+            s_t <= 0;
+        end : if_chg_state
 
-		else if (s_t != c_tmax) begin : if_not_timer_max
-			s_t <= s_t + 1;
-		end : if_not_timer_max
+        else if (s_t != c_tmax) begin : if_not_timer_max
+            s_t <= s_t + 1;
+        end : if_not_timer_max
 
 end : p_fsm_timer
 
-//FSM state register
+// FSM state register
 always_ff @(posedge i_clk)
 begin: p_fsm_state
-	if (i_rst) s_stretch_pr_state <= ST_A;
-	else s_stretch_pr_state <= s_stretch_nx_state;
+    if (i_rst) s_stretch_pr_state <= ST_A;
+    else s_stretch_pr_state <= s_stretch_nx_state;
 end : p_fsm_state
 
-//FSM combinatorial
+// FSM combinatorial
 always_comb
 begin: p_fsm_comb
-	case (s_stretch_pr_state)
-		ST_B: begin
-			/* State B. Hold the output as one and test if the time
-			   constant for stretching the output has elapsed. Upon
-			   elapse, transition back to State A. */
-			o_y = 1'b1;
-			if (s_t >= c_t_stretch - 1) s_stretch_nx_state = ST_A;
-			else s_stretch_nx_state = ST_B;
-		end
-		default: begin // ST_A
-			/* State A. Hold the output as zero and test if the input
-			   has dhanged to one. Upon change, transition to state B. */
-			o_y = 1'b0;
-			if (i_x) s_stretch_nx_state = ST_B;
-			else s_stretch_nx_state = ST_A;
-		end
-	endcase // s_stretch_pr_state
+    case (s_stretch_pr_state)
+        ST_B: begin
+            // State B. Hold the output as one and test if the time
+            // constant for stretching the output has elapsed. Upon
+            // elapse, transition back to State A.
+            o_y = 1'b1;
+            if (s_t >= c_t_stretch - 1) s_stretch_nx_state = ST_A;
+            else s_stretch_nx_state = ST_B;
+        end
+        default: begin // ST_A
+            // State A. Hold the output as zero and test if the input
+            // has dhanged to one. Upon change, transition to state B.
+            o_y = 1'b0;
+            if (i_x) s_stretch_nx_state = ST_B;
+            else s_stretch_nx_state = ST_A;
+        end
+    endcase // s_stretch_pr_state
 end : p_fsm_comb
 
 endmodule : pulse_stretcher_synch
--- ACL-Tester-Design-Verilog/RTL/pulse_stretcher_synch.v
+++ ACL-Tester-Design-Verilog/RTL/pulse_stretcher_synch.v
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------------------
 -- MIT License
 --
--- Copyright (c) 2020 Timothy Stotts
+-- Copyright (c) 2020,2022 Timothy Stotts
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
@@ -33,76 +33,76 @@
 //Timed Moore machine with timer control strategy #1
 //Part 1: Module header:--------------------------------------------------------
 module pulse_stretcher_synch(o_y, i_clk, i_rst, i_x);
-/* The exact count of clock cycles to hold Y as a one immediately after
-   a single clock cycle of X being a value of one. */
-parameter integer par_T_stretch_bits = 7; /* LOG2 of \ref par_T_stretch_val */
+// The exact count of clock cycles to hold Y as a one immediately after
+// a single clock cycle of X being a value of one.
+parameter integer par_T_stretch_bits = 7; // LOG2 of \ref par_T_stretch_val
 parameter integer par_T_stretch_val = 64;
-/* The stretched output */
+// The stretched output
 output reg o_y;
-/* Clock and reset */
+// Clock and reset
 input wire i_clk;
 input wire i_rst;
-/* The input value to stretch upon value of one. */
+// The input value to stretch upon value of one.
 input wire i_x;
 
 // Part 2: Declarations---------------------------------------------------------
-/* Stretcher FSM state values */
+// Stretcher FSM state values
 localparam [0:0] ST_A = 1'b0;
 localparam [0:0] ST_B = 1'b1;
 
-/* Stretcher FSM time constants */
+// Stretcher FSM time constants
 localparam [(par_T_stretch_bits - 1):0] c_t_stretch = par_T_stretch_val;
 localparam [(par_T_stretch_bits - 1):0] c_tmax = c_t_stretch - 1;
 
-/* Stretcher FSM state register and next state signal */
-(* fsm_encoding = "gray" *)
+// Stretcher FSM state register and next state signal
+(* fsm_encoding = "auto" *)
 (* fsm_safe_state = "default_state" *)
 reg [0:0] s_stretch_pr_state;
 reg [0:0] s_stretch_nx_state;
 
-/* Stretcher FSM timing signal */
+// Stretcher FSM timing signal
 reg [(par_T_stretch_bits - 1):0] s_t;
 
 //Part 3: Statements------------------------------------------------------------
-//Timer (strategy #1)
+// Timer (strategy #1)
 always @(posedge i_clk)
 begin: p_fsm_timer
-	if (i_rst) s_t <= 0;
-	else
-		// reset and increment the timer
-		if (s_stretch_pr_state != s_stretch_nx_state)
-			s_t <= 0;
-		else if (s_t != c_tmax)
-			s_t <= s_t + 1;
+    if (i_rst) s_t <= 0;
+    else
+        // reset and increment the timer
+        if (s_stretch_pr_state != s_stretch_nx_state)
+            s_t <= 0;
+        else if (s_t != c_tmax)
+            s_t <= s_t + 1;
 end
 
-//FSM state register
+// FSM state register
 always @(posedge i_clk)
 begin: p_fsm_state
-	if (i_rst) s_stretch_pr_state <= ST_A;
-	else s_stretch_pr_state <= s_stretch_nx_state;
+    if (i_rst) s_stretch_pr_state <= ST_A;
+    else s_stretch_pr_state <= s_stretch_nx_state;
 end
 
-//FSM combinatorial
+// FSM combinatorial
 always @(s_stretch_pr_state, i_x, s_t)
 begin: p_fsm_comb
-	case (s_stretch_pr_state)
-		ST_B: begin
-			/* State B. Hold the output as one and test if the time
-			   constant for stretching the output has elapsed. Upon
-			   elapse, transition back to State A. */
-			o_y = 1'b1;
-			if (s_t >= c_t_stretch - 1) s_stretch_nx_state = ST_A;
-			else s_stretch_nx_state = ST_B;
-		end
-		default: begin // ST_A
-			/* State A. Hold the output as zero and test if the input
-			   has dhanged to one. Upon change, transition to state B. */
-			o_y = 1'b0;
-			if (i_x) s_stretch_nx_state = ST_B;
-			else s_stretch_nx_state = ST_A;
-		end
-	endcase // s_stretch_pr_state
+    case (s_stretch_pr_state)
+        ST_B: begin
+            // State B. Hold the output as one and test if the time
+            // constant for stretching the output has elapsed. Upon
+            // elapse, transition back to State A.
+            o_y = 1'b1;
+            if (s_t >= c_t_stretch - 1) s_stretch_nx_state = ST_A;
+            else s_stretch_nx_state = ST_B;
+        end
+        default: begin // ST_A
+            // State A. Hold the output as zero and test if the input
+            // has dhanged to one. Upon change, transition to state B.
+            o_y = 1'b0;
+            if (i_x) s_stretch_nx_state = ST_B;
+            else s_stretch_nx_state = ST_A;
+        end
+    endcase // s_stretch_pr_state
 end
 
 endmodule
