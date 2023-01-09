def335ea3157d8e799b0fb8d5e1ec430b1959751
Timothy Stotts <timothystotts08@gmail.com>
2023-01-08 19:16:54 -0500

    The one_shot_fsm and pmod_cls_stand_spi_solo SystemVerilog modules were updated to use keywords of the 2012 standard instead of keywords of the 2009 standard.

--- ACL-Tester-Design-SV/RTL/pmod_cls_stand_spi_solo.sv
+++ ACL-Tester-Design-SV/RTL/pmod_cls_stand_spi_solo.sv
@@ -28,7 +28,7 @@
 -- SPI Mode 0. The design only enables clearing the display, or writing a full
 -- sixteen character line of one of the two lines of the display at a time.
 ------------------------------------------------------------------------------*/
-`begin_keywords "1800-2009"
+`begin_keywords "1800-2012"
 //------------------------------------------------------------------------------
 //Recursive Moore Machine
 //Part 1: Module header:--------------------------------------------------------
